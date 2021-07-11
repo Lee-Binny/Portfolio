16679a336b994c95aeb473da1ac7368f79d31342
이수빈 <qlsdl9516@gmail.com>
2021-07-11 23:20:30 +0900

    app 함수형 컴포넌트로 변환, data 루트 변경

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { Component } from "react";
-import ReactGA from "react-ga";
-import $ from "jquery";
+import React from "react";
+import data from "./data"
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import About from "./Components/About";
@@ -8,50 +7,20 @@ import Resume from "./Components/Resume";
 import Contact from "./Components/Contact";
 import Portfolio from "./Components/Portfolio";
 
-// TODO 함수형 컴포넌트로 수
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      foo: "bar",
-      resumeData: {}
-    };
+const App = () => {
+  //ReactGA.initialize("UA-110570651-1");
+  //ReactGA.pageview(window.location.pathname);
 
-    ReactGA.initialize("UA-110570651-1");
-    ReactGA.pageview(window.location.pathname);
-  }
-
-  getResumeData() {
-    $.ajax({
-      url: "./resumeData.json",
-      dataType: "json",
-      cache: false,
-      success: function(data) {
-        this.setState({ resumeData: data });
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.log(err);
-        alert(err);
-      }
-    });
-  }
-
-  componentDidMount() {
-    this.getResumeData();
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Header data={this.state.resumeData.main} />
-        <About data={this.state.resumeData.main} />
-        <Resume data={this.state.resumeData.resume} />
-        <Portfolio data={this.state.resumeData.portfolio} />
-        <Contact data={this.state.resumeData.main} />
-        <Footer data={this.state.resumeData.main} />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <Header data={data.main} />
+      <About data={data.main} />
+      <Resume data={data.resume} />
+      <Portfolio data={data.portfolio} />
+      <Contact data={data.main} />
+      <Footer data={data.main} />
+    </div>
+  );
 }
 
 export default App;
