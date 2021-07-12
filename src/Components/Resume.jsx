a818a2c04ea5d6beeb7312f92ff1afcd0c08fc05
이수빈 <qlsdl9516@gmail.com>
2021-07-13 00:45:13 +0900

    데이터 변경

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -19,7 +19,7 @@ const Resume = (props) => {
         <h3>{education.school}</h3>
         <p className="info">
           {education.degree} <span>&bull;</span>
-          <em className="date">{education.graduated}</em>
+          <em className="date">{education.period}</em>
         </p>
         <p>{education.description}</p>
       </div>
